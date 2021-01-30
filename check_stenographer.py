a2224e5a9e3638a76d673388ac47e6f48186db41
Roger Skjetlein <rskjetlein@netrunner.nu>
2021-01-30 22:39:36 +0100

    skel

---
+++ check_stenographer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+""" 
+Script for icinga to perform liveness check of Stenographer
+ """
+
+
+ class checkStenographer:
+    def __init__(self):
+
+    def parseStenographerConfig:
+
+    def callStenographerApi:
+    
+    def returnStatusIcinga:
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    checkStenographer()
+
+
+    
